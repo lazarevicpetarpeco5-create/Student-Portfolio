363da741d87b39f28161b266e662fee5f5b5b84b
Petar Lazarević <lazarevicpetarpeco5-create@users.noreply.github.com>
2026-09-12 14:43:51 -0400

    Set up React and Vite, starting App.jsx UI structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <>
+    <header>
+        <h1>Petar Lazarevic</h1>
+      <nav>
+       <a href="#home">Home</a>    
+       <a href="#projects">Projects</a>
+       <a href="#about">About</a>
+       <a href="#contact">Contact</a>
+      </nav>
+    </header>
+    
+    
+    
+    
+  
+
+   </>
+  )
+}
+export default App
